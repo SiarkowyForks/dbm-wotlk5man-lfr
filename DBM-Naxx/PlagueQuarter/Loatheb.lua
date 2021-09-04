f06d696b1294e06a3cd6f06167b9bf5136c46ba4
Battle <piter3110@gmail.com>
2021-09-04 15:51:21 +0200

    Loatheb 25m: fixes to aura, doom and spore timers, plus added some ground work for the upcoming healthy spore changes

--- DBM-Naxx/PlagueQuarter/Loatheb.lua
+++ DBM-Naxx/PlagueQuarter/Loatheb.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Loatheb", "DBM-Naxx", 3)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 2568 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 4443 $"):sub(12, -3))
 mod:SetCreatureID(16011)
 
 mod:RegisterCombat("combat")
@@ -14,19 +14,22 @@ mod:RegisterEvents(
 	"SWING_DAMAGE"
 )
 
-local warnSporeNow	= mod:NewSpellAnnounce(32329, 2)
-local warnSporeSoon	= mod:NewSoonAnnounce(32329, 1)
-local warnDoomNow	= mod:NewSpellAnnounce(29204, 3)
-local warnHealSoon	= mod:NewAnnounce("WarningHealSoon", 4, 48071)
-local warnHealNow	= mod:NewAnnounce("WarningHealNow", 1, 48071, false)
+local warnSporeNow		= mod:NewSpellAnnounce(32329, 2)
+local warnSporeSoon		= mod:NewSoonAnnounce(32329, 1)
+local warnDoomNow		= mod:NewSpellAnnounce(29204, 3)
+local warnHealSoon		= mod:NewAnnounce("WarningHealSoon", 4, 48071)
+local warnHealNow		= mod:NewAnnounce("WarningHealNow", 1, 48071, false)
+local warnHealthySoon 	= mod:NewAnnounce("Warning Healthy Spore Soon", 5, 35336)
+local warnHealthyNow 	= mod:NewAnnounce("Warning Healthy Spore Now", 1, 35336)
 
-
+local timerHealthy 	= mod:NewNextTimer(15, 35336)
 local timerSpore	= mod:NewNextTimer(36, 32329)
 local timerDoom		= mod:NewNextTimer(180, 29204)
 local timerAura		= mod:NewBuffActiveTimer(17, 55593)
 
 mod:AddBoolOption("SporeDamageAlert", false)
 
+local healthyTimer 	= 15
 local doomCounter	= 0
 local sporeTimer	= 36
 
@@ -34,8 +37,14 @@ function mod:OnCombatStart(delay)
 	doomCounter = 0
 	if mod:IsDifficulty("heroic25") then
 		sporeTimer = 18
+		auraTimer = 15
+		doomTimer = 90
+		timerAura:Start(auraTimer + 2 - delay)
+		timerHealthy:Start(auraTimer + 8 - delay)
 	else
 		sporeTimer = 36
+		auraTimer = 17
+		doomTimer = 120
 	end
 	timerSpore:Start(sporeTimer - delay)
 	warnSporeSoon:Schedule(sporeTimer - 5 - delay)
@@ -44,9 +53,15 @@ end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(29234) then
-		timerSpore:Start(sporeTimer)
-		warnSporeNow:Show()
-		warnSporeSoon:Schedule(sporeTimer - 5)
+		if mod:IsDifficulty("heroic25") then
+			timerSpore:Start(sporeTimer - 3)
+			warnSporeNow:Show()
+			warnSporeSoon:Schedule(sporeTimer - 8)
+		else
+			timerSpore:Start(sporeTimer)
+			warnSporeNow:Show()
+			warnSporeSoon:Schedule(sporeTimer - 5)
+		end
 	elseif args:IsSpellID(29204, 55052) then  -- Inevitable Doom
 		doomCounter = doomCounter + 1
 		local timer = 30
@@ -57,9 +72,13 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnDoomNow:Show(doomCounter)
 		timerDoom:Start(timer, doomCounter + 1)
 	elseif args:IsSpellID(55593) then
-		timerAura:Start()
+		if mod:IsDifficulty("normal10") then
 		warnHealSoon:Schedule(14)
 		warnHealNow:Schedule(17)
+		else
+		
+		end
+		timerAura:Start(auraTimer)
 	end
 end
 
