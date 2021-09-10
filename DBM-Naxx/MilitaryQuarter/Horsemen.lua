0210aa9bad79f23f07062bc7fd02960a489849bb
Exeasd <44117475+Exeasd@users.noreply.github.com>
2021-09-10 17:22:10 +0200

    Add Meteor Timer, Holy Wrath Timer, Void Zone Timer, Range Check for Holy Wrath chain range, All Bosses Enrage Timers Also added IsSpellName function to core.

--- DBM-Naxx/MilitaryQuarter/Horsemen.lua
+++ DBM-Naxx/MilitaryQuarter/Horsemen.lua
@@ -10,7 +10,11 @@ mod:EnableModel()
 
 mod:RegisterEvents(
 	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED_DOSE"
+	"SPELL_AURA_APPLIED_DOSE",
+	"CHAT_MSG_MONSTER_YELL",
+	"SPELL_SUMMON",
+	"SPELL_CAST_START",
+	"SPELL_DAMAGE"
 )
 
 local warnMarkSoon			= mod:NewAnnounce("WarningMarkSoon", 1, 28835, false)
@@ -19,6 +23,15 @@ local warnMarkNow			= mod:NewAnnounce("WarningMarkNow", 2, 28835)
 local specWarnMarkOnPlayer	= mod:NewSpecialWarning("SpecialWarningMarkOnPlayer", nil, false, true)
 
 mod:AddBoolOption("HealthFrame", true)
+mod:AddBoolOption("ShowRange", true)
+
+local timerBlaumeux			= mod:NewTimer(309, "TimerLadyBlaumeuxEnrage", 72143)
+local timerZeliek			= mod:NewTimer(309, "TimerSirZeliekEnrage", 72143)
+local timerKorthazz			= mod:NewTimer(309, "TimerThaneKorthazzEnrage", 72143)
+local timerRivendare		= mod:NewTimer(309, "TimerBaronRivendareEnrage", 72143)
+local timerVoidZone			= mod:NewCDTimer(12, 36119)
+local timerMeteor			= mod:NewCDTimer(12, 28884)
+local timerHolyWrath		= mod:NewCDTimer(12, 57466)
 
 mod:SetBossHealthInfo(
 	16064, L.Korthazz,
@@ -30,7 +43,10 @@ mod:SetBossHealthInfo(
 local markCounter = 0
 
 function mod:OnCombatStart(delay)
+	timerVoidZone:Start(16 - delay)
 	markCounter = 0
+	timerBlaumeux:Start()
+	timerRivendare:Start()
 end
 
 local markSpam = 0
@@ -38,6 +54,32 @@ function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(28832, 28833, 28834, 28835) and (GetTime() - markSpam) > 5 then
 		markSpam = GetTime()
 		markCounter = markCounter + 1
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == "Death... will not stop me." or msg:find("Death... will not stop me.") then
+		timerKorthazz:Start()
+		timerRivendare:Stop()
+		timerMeteor:Start(30)
+	elseif msg == "Touche..." or msg:find("Touche...") then
+		if mod:IsDifficulty("normal10")	then
+			timerVoidZone:Stop()
+		end
+		if self.Options.ShowRange then
+			self:RangeToggle(true)
+		end
+		timerZeliek:Start()
+		timerHolyWrath:Start()
+		timerBlaumeux:Stop()
+	elseif msg == "It is... as it should be." or msg:find("It is... as it should be.") then
+		if self.Options.ShowRange then
+			self:RangeToggle(false)
+		end
+		timerZeliek:Stop()
+		timerHolyWrath:Stop()
+	elseif msg == "What a bloody waste this is!" or msg:find("What a bloody waste this is!") then
+		timerKorthazz:Stop()
 	end
 end
 
@@ -49,3 +91,34 @@ function mod:SPELL_AURA_APPLIED_DOSE(args)
 	end
 end
 
+function mod:SPELL_SUMMON(args)
+	if args:IsSpellName("Void Zone") then
+		timerVoidZone:Start()
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellName("Meteor") then
+		timerMeteor:Start()
+	end
+end
+
+function mod:SPELL_DAMAGE(args)
+	if args:IsSpellName("Holy Wrath") then
+		timerHolyWrath:Start()
+	end
+end
+
+function mod:RangeToggle(show)
+	if show then
+		DBM.RangeCheck:Show(10)
+	else
+		DBM.RangeCheck:Hide()
+	end
+end
+
+function mod:OnCombatEnd()
+	if self.Options.ShowRange then
+		self:RangeToggle(false)
+	end
+end
