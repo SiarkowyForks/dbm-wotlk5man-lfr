13631a2c00031bb45d87903d86e21b5c24af55cc
Exeasd <44117475+Exeasd@users.noreply.github.com>
2021-09-11 19:41:54 +0200

    feat(Naxxramas25/Gluth): Add Flesh Rip warning and icons on target

--- DBM-Naxx/ConstructQuarter/Gluth.lua
+++ DBM-Naxx/ConstructQuarter/Gluth.lua
@@ -3,24 +3,37 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 2869 $"):sub(12, -3))
 mod:SetCreatureID(15932)
+mod:SetUsedIcons(7, 8)
 
 mod:RegisterCombat("combat")
 
 mod:EnableModel()
 
 mod:RegisterEvents(
-	"SPELL_DAMAGE"
+	"SPELL_DAMAGE",
+	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_REMOVED"
 )
 
 
 local warnDecimateSoon	= mod:NewSoonAnnounce(54426, 2)
 local warnDecimateNow	= mod:NewSpellAnnounce(54426, 3)
+local specwarnFleshRip	= mod:NewSpecialWarning("SpecialWarningFleshRip")
+local warnFleshRip		= mod:NewTargetAnnounce(40199, 2)
 
-local enrageTimer		= mod:NewBerserkTimer(420)
+local enrageTimer		= mod:NewBerserkTimer(480)
 local timerDecimate		= mod:NewCDTimer(104, 54426)
 
+mod:AddBoolOption("SetIconOnFleshRipTarget", true)
+
+local fleshripIcons = {}
+
 function mod:OnCombatStart(delay)
-	enrageTimer:Start(420 - delay)
+	if mod:IsDifficulty("heroic25") then
+		enrageTimer:Start(-delay)
+	else
+		enrageTimer:Start(420 - delay)
+	end
 	timerDecimate:Start(110 - delay)
 	warnDecimateSoon:Schedule(100 - delay)
 end
@@ -35,4 +48,45 @@ function mod:SPELL_DAMAGE(args)
 	end
 end
 
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(40199) then
+		warnFleshRip:Show(args.destName)
+		if args:IsPlayer() then
+			specwarnFleshRip:Show()
+		end
+		if self.Options.SetIconOnFleshRipTarget then
+			table.insert(fleshripIcons, args.destName)
+			addIcon()
+		end
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	if args:IsSpellID(40199) and self.Options.SetIconOnInjectionTarget then
+		removeIcon(args.destName)
+	end
+end
+
+local function addIcon()
+	for i,j in ipairs(fleshripIcons) do
+		local icon = 9 - i
+		mod:SetIcon(j, icon)
+	end
+end
+
+local function removeIcon(target)
+	for i,j in ipairs(fleshripIcons) do
+		if j == target then
+			table.remove(fleshripIcons, i)
+			mod:SetIcon(target, 0)
+		end
+	end
+	addIcon()
+end
+
+function mod:OnCombatEnd()
+    for i,j in ipairs(fleshripIcons) do
+       mod:SetIcon(j, 0)
+    end
+end
 
