e7eeb4a39f42544b40428d033c47a0449dacf71d
Siarkowy <Siarkowy@users.noreply.github.com>
2021-09-14 17:32:08 +0000

    chore: Bump revision

--- DBM-ChamberOfAspects/Obsidian/Sartharion.lua
+++ DBM-ChamberOfAspects/Obsidian/Sartharion.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Sartharion", "DBM-ChamberOfAspects", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 4450 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 4451 $"):sub(12, -3))
 mod:SetCreatureID(28860)
 mod:SetZone()
 
