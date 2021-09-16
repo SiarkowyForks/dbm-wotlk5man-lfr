898f037c64fda67485cfa1f481ac8f3483b7459b
Siarkowy <Siarkowy@users.noreply.github.com>
2021-09-16 23:41:18 +0000

    chore: Bump revision

--- DBM-Naxx/PlagueQuarter/Loatheb.lua
+++ DBM-Naxx/PlagueQuarter/Loatheb.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Loatheb", "DBM-Naxx", 3)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 4451 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 4455 $"):sub(12, -3))
 mod:SetCreatureID(16011)
 
 mod:RegisterCombat("combat")
