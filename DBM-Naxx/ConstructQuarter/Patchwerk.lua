b994d1d484b09fb8aaf777eaf102a9df08ffb73d
Siarkowy <Siarkowy@users.noreply.github.com>
2021-09-20 13:04:24 +0000

    chore: Bump revision

--- DBM-Naxx/ConstructQuarter/Patchwerk.lua
+++ DBM-Naxx/ConstructQuarter/Patchwerk.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Patchwerk", "DBM-Naxx", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 4456 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 4457 $"):sub(12, -3))
 mod:SetCreatureID(16028)
 mod:SetUsedIcons(5, 6, 7, 8)
 
