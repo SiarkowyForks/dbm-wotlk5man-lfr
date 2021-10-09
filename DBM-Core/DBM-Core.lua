6a6ee7fe4be40b87b3f17e212c0f63c580adf191
Siarkowy <Siarkowy@users.noreply.github.com>
2021-10-09 15:56:06 +0000

    chore: Bump revision [skip ci]

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 -------------------------------
 --  Globals/Default Options  --
 -------------------------------
-DBM_REVISION = "$Revision: 4460 $"
+DBM_REVISION = "$Revision: 4461 $"
 
 DBM = {
 	Revision = DBM_REVISION:sub(12, -3),
